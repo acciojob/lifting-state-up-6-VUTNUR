de85d50979702cf359548113f31b2c593b8a1cc0
VUTNUR <vutnurnethajigoud@gmail.com>
2023-07-19 12:09:36 +0530

    donw1

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,18 +2,29 @@ import React,{useState} from "react";
 
 const Child=({value,func})=>{
         function remove(e){
-            let result=value.filter((elem,index)=>{
-                 return e.target.className!=index;
+            let result=value.map((elem,index)=>{
+                 if(e.target.className==index){
+                    return {...elem,completed: true}
+                 }
+                 return elem;
             })
             func(result);
         }
     return(
         <div>
+            <h3>Child Component</h3>
             <ul>
             {
                 value.map((elem,index)=>{
                   return  (
-                        <li>{elem}<button  className={index} onClick={remove}>Complete</button></li>
+                    <li>{elem.text}
+                    
+                     {
+                        !elem.completed &&
+                        <button  className={index} onClick={remove}>Complete</button>
+                     }  
+                       </li>
+                       
                     )
                      } )
             }
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,7 +2,10 @@ import React,{useState} from "react";
 import Child from "./Child"
 
 const Parent=()=>{
-   let [todos, setTodos]=useState(["Learn React","Build a React app","Deploy the React app"]);
+   let [todos, setTodos]=useState([{ id: 1, text: 'Learn React', completed: false },
+   { id: 2, text: 'Build a todo app', completed: false },
+   { id: 3, text: 'Deploy to production', completed: false }]
+);
     return (
         <div>
             <h1>Parent Component</h1>
